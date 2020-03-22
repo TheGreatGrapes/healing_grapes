b4211450a41e0674a788bd2b545e9495262227df
Debaraj Barua <debaraj-barua@users.noreply.github.com>
2020-03-22 15:24:13 +0100

    [feat] map functionalities (#1)
    * geo locate user

--- saving_grapes/urls.py
+++ saving_grapes/urls.py
@@ -1,4 +1,4 @@
-"""healing_grapes URL Configuration
+"""saving_grapes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- saving_grapes/wsgi.py
+++ saving_grapes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for healing_grapes project.
+WSGI config for saving_grapes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healing_grapes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saving_grapes.settings')
 
 application = get_wsgi_application()
